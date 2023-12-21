5d67fcf64bbb36df7f4de487d8177a8da3055e1d
Jordan <contact@jordan-chap.com>
2023-12-22 00:47:54 +0100

    testing map for parsing OK

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jordan <jordan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 15:12:15 by jsousa-a          #+#    #+#             */
-/*   Updated: 2023/12/21 23:29:36 by jordan           ###   ########.fr       */
+/*   Updated: 2023/12/22 00:45:42 by jordan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,9 @@ int	main(int ac, char **av)
 	printf("size.x: %d\n", level.data.file_size.x);
 	printf("size.y: %d\n", level.data.file_size.y);
 	printf("name: %s\n", level.name);
+	printf("texture[0].name: %s\n", level.data.texture[0].name);
+	printf("texture[0].path: %s\n", level.data.texture[0].path);
+	printf("map[0]: %s\n", level.map[0]);
+	printf("map[0]: %s\n", level.map[13]);
 	return (0);
 }
